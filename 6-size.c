c3e68dc468ae724bd0ec48456837487968a36e21
Ainonangula <114227170+Ainonangula@users.noreply.github.com>
2022-10-15 14:11:03 +0200

    Create 6-size.c

---
+++ 6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (void) {int a;long int b;long long
+int c;char d; float f;
+print f ("size of a char:%lu byte(s)\n",
+(unsigned long) size of (d)); 
+
+print f ("size of an int:%lu byte(s)\n",
+(unsigned long) size of (a)) ;
+
+print f ("size of a long int:%lu byte(s)\n",
+(unsigned long) size of (b)) ;
+
+print f ("size of a long long int:%lu byte(s)\n",
+(unsigned long) size of (c)) ;
+
+print f ("size of a float :%lu byte(s)\n",
+(unsigned long) size of (f)) ;
+
+return(0);
+}} 
